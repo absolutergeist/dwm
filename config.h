1944cc2deddaa1e169786d2b2144213407e908c3
absolutergeist <rafael.rodrigues@tuta.io>
2021-11-30 16:51:55 +0100

    Add tag mask 9 to Anki & remove rules for Gimp and Firefox
    Removing rules for Gimp and Firefox for the sake of code tidiness

--- config.h
+++ config.h
@@ -27,9 +27,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            1,           -1 },
-	{ "Anki",     NULL,       NULL,       0,            1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           -1 },
+	{ "Anki",     NULL,       NULL,       1 << 8,       1,           -1 },
 };
 
 /* layout(s) */
